401d310c3edd7ccca42b90b99da82fadcd7fcf64
laijia <laijia2008@126.com>
2016-03-19 20:21:53 +0800

    update the TestBase64.cpp

--- TestBase64.cpp
+++ TestBase64.cpp
@@ -6,37 +6,37 @@ using namespace std;
   
 int main()  
 {  
-    // unsigned long len = 0;  
+    unsigned long len = 0;  
   
-    // unsigned char pIn[100];  
-    // unsigned char pOut[200];  
+    unsigned char pIn[100];  
+    unsigned char pOut[200];  
 
-	// char  str[] = "15122840452";
-    // memcpy(pIn,str, sizeof(str));  
+	char  str[] = "laijia200815122840452";
+    memcpy(pIn,str, sizeof(str));  
   
-    // string strout;  
+    string strout;  
   
-    // cout<<(char*)pIn<<endl;  
-    // if(CBase64::Encode(pIn, sizeof(str), strout))  
-    //     cout<<strout<<endl;  
-    // else  
-    //     cout<<"encode err"<<endl;  
+    cout<<(char*)pIn<<endl;  
+    if(CBase64::Encode(pIn, strlen(str), strout))  
+        cout<<strout<<endl;  
+    else  
+        cout<<"encode err"<<endl;  
   
-    // string stroIn = strout;  
-    // cout<<stroIn<<endl;  
+    string stroIn = strout;  
+    cout<<stroIn<<endl;  
   
-    // memset(pOut,0,sizeof(pOut));  
+    memset(pOut,0,sizeof(pOut));  
   
-    // if(CBase64::Decode(stroIn, pOut, &len))  
-    // {  
-    //     cout<<(char *)pOut<<endl;  
-    //     cout<<"len="<<strlen((char *)pOut)<<endl;  
-    // }  
-    // else  
-    //     cout<<"deconde err"<<endl;  
+    if(CBase64::Decode(stroIn, pOut, &len))  
+    {  
+        cout<<(char *)pOut<<endl;  
+        cout<<"len="<<strlen((char *)pOut)<<endl;  
+    }  
+    else  
+        cout<<"deconde err"<<endl;  
   
-    // cout<<len<<endl;  
-	const std::string s = "laijia2008" ;  
+    cout<<len<<endl;  
+	const std::string s = "laijia200815122840452" ;  
   
 	std::string encoded = base64_encode(reinterpret_cast<const unsigned char*>(s.c_str()), s.length());  
 	std::string decoded = base64_decode(encoded);  
